a3144170c4d533f3bb972143244b60b14e473c93
Eligenaut <brunobrayane@gmail.com>
2025-07-01 03:30:13 +0300

    Ajout du backend Laravel pour déploiement Railway

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Installer les dépendances PHP
 composer install --no-dev --optimize-autoloader
-
-# Lancer les migrations (facultatif)
 php artisan migrate --force
-
-# Lancer le serveur Laravel sur le bon port
-php artisan serve --host=0.0.0.0 --port=${PORT}
+php artisan serve --host 0.0.0.0 --port $PORT
